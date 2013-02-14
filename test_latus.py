0773458946ec35902158a9c34f5c11e31e9bd0b4
jabel <james.c.abel@intel.com>
2013-02-13 20:02:10 -0700

    dynamically create test files

--- test_latus.py
+++ test_latus.py
@@ -3,12 +3,13 @@
 
 import os
 
-SIMPLE = "simple"
-SRC = "src"
-DEST_EMPTY = "dest_empty"
-DEST_EXISTS_EXACT = "dest_exists_exact"
-DEST_EXISTS_DIFFERENT = "dest_exists_different"
-DEST_EXISTS_UNDER_DIFFERENT_NAME = "dest_exists_under_different_name"
+SIMPLE = u"simple"
+TEST_UNICODE = u"unicode"
+SRC = u"src"
+DEST_EMPTY = u"dest_empty"
+DEST_EXISTS_EXACT = u"dest_exists_exact"
+DEST_EXISTS_DIFFERENT = u"dest_exists_different"
+DEST_EXISTS_UNDER_DIFFERENT_NAME = u"dest_exists_under_different_name"
 
 TEST_ROOT = "test"
 
@@ -17,10 +18,11 @@ def get_test_root():
 
 # use this instead of logger.py's get_log_file_path() so the log file is put in the proper place for testing
 def get_log_file_path():
-    return os.path.join(TEST_ROOT, __name__  + ".log")
+    return os.path.join(TEST_ROOT, __name__  + u".log")
 
 # note that this makes the required dirs if necessary
 def write_to_file(p, contents):
+    print "p", p
     make_dirs(os.path.split(p)[0])
     f = open(p, "w")
     f.write(contents)
@@ -30,14 +32,35 @@ def make_dirs(p):
     if not os.path.exists(p):
         os.makedirs(p)
 
+def make_unicode_string(start, inc, length):
+    s = u''
+    c = start
+    # Avoid / and \ so we don't mistakenly create a folder, as well as other illegal filename chars
+    illegal_chars = [ u'/', u"\\", u";", u"*", u"?", u'"', u"<", u">", u"|", u":"]
+    while len(s) < length:
+        if (c >= 32) and not (unichr(c) in illegal_chars):
+            s = s + unichr(c)
+        c = c + inc
+    return s
+
+def get_unicode_test_filename():
+    # todo : dynamically determine how many chars we can go to in this total path length  ... there seems to be a limit
+    return u'A' + make_unicode_string(0, 1, 217) + u'.txt'
+
 # This writes various input files.  The goal is to not have to package up test files in the repo, if we
 # can avoid it.  Also, this way we can readily re-initialize and fully clean up test files.
 def write_files():
     test_string = "a"
     different_test_string = "b"
+    a_file_name = u"a.txt"
     simple_path = os.path.join(get_test_root(), SIMPLE)
-    write_to_file(os.path.join(simple_path, SRC, "a.txt"), test_string)
+    write_to_file(os.path.join(simple_path, SRC, a_file_name), test_string)
     make_dirs(os.path.join(simple_path, DEST_EMPTY))
-    write_to_file(os.path.join(simple_path, DEST_EXISTS_EXACT, "a.txt"), test_string)
-    write_to_file(os.path.join(simple_path, DEST_EXISTS_DIFFERENT, "a.txt"), different_test_string)
-    write_to_file(os.path.join(simple_path, DEST_EXISTS_UNDER_DIFFERENT_NAME, "a_but_different_name.txt"), test_string)
+    write_to_file(os.path.join(simple_path, DEST_EXISTS_EXACT, a_file_name), test_string)
+    write_to_file(os.path.join(simple_path, DEST_EXISTS_DIFFERENT, a_file_name), different_test_string)
+    write_to_file(os.path.join(simple_path, DEST_EXISTS_UNDER_DIFFERENT_NAME, u"a_but_different_name.txt"), test_string)
+    # todo : make the contents unicode characters (write doesn't seem to allow non-ASCII)
+    write_to_file(os.path.join(get_test_root(), TEST_UNICODE, get_unicode_test_filename()), test_string)
+
+if __name__ == "__main__":
+    write_files()
--- test_merge.py
+++ test_merge.py
@@ -7,46 +7,53 @@ import merge
 class test_merge(unittest.TestCase):
     def setUp(self):
         test_latus.write_files()
-        self.test_root = os.path.join(test_latus.TEST_ROOT, test_latus.SIMPLE)
+        self.simple_root = os.path.join(test_latus.TEST_ROOT, test_latus.SIMPLE)
+        self.unicode_root = os.path.join(test_latus.TEST_ROOT, test_latus.TEST_UNICODE)
         self.a_file = "a.txt"
-        self.src_root = os.path.join(self.test_root, test_latus.SRC)
-        self.out_file_path = os.path.join(self.test_root, "domerge.bat")
-        m = merge.merge(self.src_root, self.out_file_path, verbose=True, metadata_root_override=self.test_root, get_log_file_path = test_latus.get_log_file_path)
-        m.clean() # start with a fresh database
+        self.src_root = os.path.join(self.simple_root, test_latus.SRC)
+        self.out_file_path = os.path.join(self.simple_root, "domerge.bat")
+        self.create_merge_object()
+        self.m.clean() # start with a fresh database
         # Load up the database with file info.  Generally this is run in run(), but since we're
         # doing one file at at file for some of these tests we need to do this separately.
-        m.scan(self.test_root)
+        self.m.scan(self.simple_root)
 
     def tearDown(self):
         del self.m
 
+    # note that this is dest then src, since usually we change the dest so it has to be first
+    def create_merge_object(self, dest_root = None, src_root = None):
+        if src_root is None:
+            src_root = self.src_root
+        self.m = merge.merge(src_root, self.out_file_path, dest_root, True, test_latus.TEST_ROOT, get_log_file_path = test_latus.get_log_file_path)
+
     # a single file
     def test_single_file_does_not_exist(self):
-        dest_root = os.path.join(self.test_root, test_latus.DEST_EMPTY)
-        self.m = merge.merge(self.src_root, self.out_file_path, dest_root, True, self.test_root, get_log_file_path = test_latus.get_log_file_path)
+        self.create_merge_object(os.path.join(self.simple_root, test_latus.DEST_EMPTY))
         search_result, search_paths = self.m.merge_file(self.a_file)
         assert(search_result == merge.DOES_NOT_EXIST)
 
     def test_single_file_exists_exact(self):
-        dest_root = os.path.join(self.test_root, test_latus.DEST_EXISTS_EXACT)
-        self.m = merge.merge(self.src_root, self.out_file_path, dest_root, True, self.test_root, get_log_file_path = test_latus.get_log_file_path)
+        self.create_merge_object(os.path.join(self.simple_root, test_latus.DEST_EXISTS_EXACT))
         search_result, search_paths = self.m.merge_file(self.a_file)
         assert(search_result == merge.EXISTS_EXACT)
 
     def test_single_file_exists_different(self):
-        dest_root = os.path.join(self.test_root, test_latus.DEST_EXISTS_DIFFERENT)
-        self.m = merge.merge(self.src_root, self.out_file_path, dest_root, True, self.test_root, get_log_file_path = test_latus.get_log_file_path)
+        self.create_merge_object(os.path.join(self.simple_root, test_latus.DEST_EXISTS_DIFFERENT))
         search_result, search_paths = self.m.merge_file(self.a_file)
         assert(search_result == merge.EXISTS_CONFLICT)
 
     def test_single_file_exists(self):
-        dest_root = os.path.join(self.test_root, test_latus.DEST_EXISTS_UNDER_DIFFERENT_NAME)
-        self.m = merge.merge(self.src_root, self.out_file_path, dest_root, True, self.test_root, get_log_file_path = test_latus.get_log_file_path)
+        self.create_merge_object(os.path.join(self.simple_root, test_latus.DEST_EXISTS_UNDER_DIFFERENT_NAME))
         search_result, search_paths = self.m.merge_file(self.a_file)
         assert(search_result == merge.EXISTS_ELSEWHERE)
 
+    def test_unicode_filename(self):
+        self.create_merge_object(None, self.unicode_root)
+        self.m.scan(self.unicode_root)
+
     def test_run(self):
-        self.m = merge.merge(self.src_root, self.out_file_path, os.path.join(self.test_root, "dest_empty"), True, self.test_root, get_log_file_path = test_latus.get_log_file_path)
+        self.create_merge_object(os.path.join(self.simple_root, "dest_empty"), self.simple_root)
         self.m.run()
         # todo: what do I test for?
 
