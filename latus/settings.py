3fef85785ba98f6499bfb157462a984957e44cc6
jabel <james.c.abel@intel.com>
2013-07-18 00:38:24 -0700

    some more settings simplifications

--- latus/settings.py
+++ latus/settings.py
@@ -13,31 +13,32 @@ class Settings:
         self.file_path = os.path.join(app_data_folder, const.SETTINGS_FILE_NAME)
         self.log = logger.get_log()
 
+    # if does not exists, may want to take the user through setup
+    def exists(self):
+        return os.path.exists(self.file_path)
+
     # some simple section and key checking
     def check(self, section, key):
         sects_and_keys = {'folder' : ['cloud', 'local']}
         if section not in sects_and_keys:
-            raise Exception("invalid section: %s" % section)
+            raise Exception("bad section: %s" % section)
         if key not in sects_and_keys[section]:
-            raise Exception("invalid key: %s" % key)
+            raise Exception("bad key: %s" % key)
 
     def read(self):
-        exists = False
-        self.config = configparser.ConfigParser() # start anew each time we read the settings in case the file changed
-        if os.path.exists(self.file_path):
+        config_exists = False
+        self.config = configparser.ConfigParser() # start anew each time in case the file changed
+        if self.exists():
             self.config.read(self.file_path)
-            exists = True
-        return exists
+            config_exists = True
+        return config_exists
 
-    # read in all entries
+    # read in all entries (good for dumping all settings)
     def get_all(self):
         self.read()
         settings = {}
-        for section in self.config.sections():
-            settings[section] = {}
-            for key in self.config.options(section):
-                val = self.config.get(section, key)
-                settings[section][key] = val
+        for section_name, section_proxy in self.config.items():
+            settings[section_name] = self.config.items(section_name)
         return settings
 
     # get one section/key entry
@@ -48,7 +49,7 @@ class Settings:
             if self.config.has_option(section, key):
                 val = self.config.get(section, key)
         if val is None:
-            self.log.warn("invalid : section=%s key=%s" % (section, key))
+            self.log.warn("can not get : section=%s key=%s" % (section, key))
         return val
 
     def set(self, section, key, value):
