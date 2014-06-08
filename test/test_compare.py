6c1a067187b5af38c2fecd67b515bce69ffcfa74
James Abel <james.c.abel@intel.com>
2014-06-07 22:54:34 -0700

    update tests since DB no longer holds root

--- test/test_compare.py
+++ test/test_compare.py
@@ -10,13 +10,13 @@ import test.create_files
 from test.conftest import setup
 
 def test_scan_compare(setup):
-    metadata_path = os.path.join(test.create_files.get_metadata_root(), 'diff')
+    metadata_path = os.path.join(test.create_files.get_metadata_root(), 'compare')
     x_folder = os.path.join(test.create_files.get_compare_root(), test.const.X_FOLDER)
     y_folder = os.path.join(test.create_files.get_compare_root(), test.const.Y_FOLDER)
-    dbx = core.db.DB(x_folder, core.metadatapath.MetadataPath(metadata_path), force_drop=True)
-    dbx.scan()
-    dby = core.db.DB(y_folder, core.metadatapath.MetadataPath(metadata_path))
-    dby.scan()
+    dbx = core.db.DB(core.metadatapath.MetadataPath(metadata_path), force_drop=True)
+    dbx.scan(x_folder)
+    dby = core.db.DB(core.metadatapath.MetadataPath(metadata_path))
+    dby.scan(y_folder)
     a_minus_b, intersection = dbx.compare(x_folder, y_folder)
     assert(a_minus_b == [test.create_files.A_FILE_NAME])
     assert(intersection == [test.create_files.B_FILE_NAME])
--- test/test_scan.py
+++ test/test_scan.py
@@ -3,34 +3,34 @@ import os
 import core.const
 import core.db
 import core.metadatapath
+import test.const
 import test.util
 import test.results
 import test.create_files
 from test.conftest import setup
 
 def test_scan_simple(setup):
-    db = core.db.DB(test.create_files.get_simple_root(), core.metadatapath.MetadataPath(test.create_files.get_metadata_root()), force_drop=True)
-    db.scan()
+    db = core.db.DB(core.metadatapath.MetadataPath(test.create_files.get_metadata_root()), force_drop=True)
+    db.scan(test.create_files.get_simple_root())
     a_path = os.path.join(test.create_files.SRC, test.create_files.A_FILE_NAME)
     assert(a_path != None)
     if a_path is not None:
         sha512_val = db.get_file_info(a_path).sha512
         assert(sha512_val == test.results.sha512[test.create_files.A_STRING])
-    assert(db.get_root() == os.path.abspath(test.create_files.get_simple_root()))
 
 def test_scan_unicode(setup):
-    db = core.db.DB(test.create_files.get_unicode_root(), core.metadatapath.MetadataPath(test.create_files.get_metadata_root()), force_drop=True)
-    db.scan()
+    db = core.db.DB(core.metadatapath.MetadataPath(test.create_files.get_metadata_root()), force_drop=True)
+    db.scan(test.create_files.get_unicode_root())
 
 def test_new_root(setup):
-    db = core.db.DB(test.create_files.get_simple_root(), core.metadatapath.MetadataPath(test.create_files.get_metadata_root()), force_drop=True)
-    db.scan()
-    db = core.db.DB(test.create_files.get_unicode_root(), core.metadatapath.MetadataPath(test.create_files.get_metadata_root()))
-    db.scan()
+    db = core.db.DB(core.metadatapath.MetadataPath(test.create_files.get_metadata_root()), force_drop=True)
+    db.scan(test.create_files.get_simple_root())
+    db = core.db.DB(core.metadatapath.MetadataPath(test.create_files.get_metadata_root()))
+    db.scan(test.create_files.get_unicode_root())
 
 def test_hash_time(setup):
-    db = core.db.DB(test.create_files.get_hash_root(), core.metadatapath.MetadataPath(os.path.join(test.create_files.get_metadata_root(), 'hashtime')))
-    db.scan()
+    db = core.db.DB(core.metadatapath.MetadataPath(os.path.join(test.create_files.get_metadata_root(), 'hashtime')))
+    db.scan(test.create_files.get_hash_root())
     hash_perfs = db.get_hash_perf()
     assert(len(hash_perfs) == core.const.MAX_HASH_PERF_VALUES) # make sure the table is full
     files_in_hash_perf = ['big' + str(i) + '.txt' for i in range(core.const.MAX_HASH_PERF_VALUES,test.const.HASH_TEST_FILE_MAX+1)]
