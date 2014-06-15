e2cc789c46e4929682847b8b300342feeb7b7e6f
James Abel <james.c.abel@intel.com>
2014-06-14 22:54:03 -0700

    started sync capability

--- core/sync.py
+++ core/sync.py
@@ -1,1 +1,20 @@
-__author__ = 'jabel'
+
+import os
+
+import core.db
+
+class Sync():
+    """
+    Determines what needs to be done to sync local to cloud.
+    Assumes local already scanned (see db.scan() ).
+    """
+    def __init__(self, local_db, sync_db, cloud_db):
+        self.local_db = local_db
+        self.sync_db = sync_db
+        self.cloud_db = cloud_db
+
+    def sync(self):
+        for f in self.local_db:
+            print('f', f.abspath)
+        # todo: the actual sync!
+
