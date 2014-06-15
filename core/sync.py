1d9cf969b79d6124c1b023abde25ab2683a86548
James Abel <james.c.abel@intel.com>
2014-06-15 12:44:17 -0700

    general development on sync module

--- core/sync.py
+++ core/sync.py
@@ -8,13 +8,18 @@ class Sync():
     Determines what needs to be done to sync local to cloud.
     Assumes local already scanned (see db.scan() ).
     """
-    def __init__(self, local_db, sync_db, cloud_db):
-        self.local_db = local_db
-        self.sync_db = sync_db
-        self.cloud_db = cloud_db
+    def __init__(self, metadata_folder, local_folder, node_id):
+        self.local_folder = local_folder
+        self.db = core.db.DB(core.metadatapath.MetadataPath(metadata_folder), node_id, force_drop=True)
+
+    def scan(self):
+        self.db.scan(self.local_folder)
 
     def sync(self):
-        for f in self.local_db:
+        for f in self.db:
             print('f', f.abspath)
         # todo: the actual sync!
+
+    def close(self):
+        self.db.close()
 
