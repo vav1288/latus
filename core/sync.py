68ce178d5e00056ae342b303c4be4199234ea52d
James Abel <james.c.abel@intel.com>
2014-06-19 18:36:27 -0700

    set up the local, cloud and sync databases here

--- core/sync.py
+++ core/sync.py
@@ -10,16 +10,18 @@ class Sync():
     """
     def __init__(self, metadata_folder, local_folder, node_id):
         self.local_folder = local_folder
-        self.db = core.db.DB(core.metadatapath.MetadataPath(metadata_folder), node_id, force_drop=True)
+        self.ldb = core.db.DB(core.metadatapath.MetadataPath(metadata_folder), node_id, force_drop=True) # local
+        self.cdb = core.db.DB(core.metadatapath.MetadataPath(metadata_folder), 'c', force_drop=True) # cloud
+        self.sdb = core.db.DB(core.metadatapath.MetadataPath(metadata_folder), 's', force_drop=True) # sync
 
     def scan(self):
-        self.db.scan(self.local_folder)
+        self.ldb.scan(self.local_folder) # scan the local folder
 
     def sync(self):
-        for f in self.db:
+        for f in self.ldb:
             print('f', f.abspath)
         # todo: the actual sync!
 
     def close(self):
-        self.db.close()
+        self.ldb.close()
 
