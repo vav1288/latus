b4d0d1a1391434a74decc97d86d847e07de6a23b
James Abel <james.c.abel@intel.com>
2014-06-19 18:37:39 -0700

    don't need the cloud param from the user

--- sync.py
+++ sync.py
@@ -10,11 +10,10 @@ if __name__ == "__main__":
     parser  = argparse.ArgumentParser()
     parser.add_argument('-m', '--metadata', metavar='path', required=True, help='metadata root')
     parser.add_argument('-l', '--local', metavar='path', required=True, help="local folder")
-    parser.add_argument('-c', '--cloud', metavar='path', required=True, help="cloud folder")
     parser.add_argument('-i', '--id', metavar='path', required=True, help="identifier for this 'node'")
     parser.add_argument('-v', '--verbose', action='store_true', help="output status messages during execution")
     args = parser.parse_args()
 
-    sync = core.sync.Sync(args.metadata, args.id)
+    sync = core.sync.Sync(args.metadata, args.local, args.id)
     sync.scan()
     sync.sync()
