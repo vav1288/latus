bec25ddba926a75384febf35ad4463fd8e351aa9
James Abel <james.c.abel@intel.com>
2014-06-21 11:05:20 -0700

    make metadatapath deal better with both local and cloud storage

--- core/metadatapath.py
+++ core/metadatapath.py
@@ -6,18 +6,24 @@ import core.util
 import win32api, win32con
 import functools
 
+CLOUD = 1  # cloud storage side- dropbox, etc.
+LOCAL = 2  # local - e.g. appdata in Windows
+
 class MetadataPath():
     """
     Helper for creating and managing the metadata folder/dir
     """
-    METADATA_DIR_NAME = "." + core.const.NAME
     DB_DIR_NAME = 'db'
 
-    def __init__(self, metadata_base):
+    def __init__(self, metadata_base, type = LOCAL):
+        if type == CLOUD:
+            self.metadata_dir_name = "." + core.const.NAME  # put a dot in front for cloud folder
+        elif type == LOCAL:
+            self.metadata_dir_name = core.const.NAME  # no dot for local folder
         self.metadata_base = metadata_base
         if not os.path.exists(self.db_folder):
             os.makedirs(self.db_folder)
-        core.util.make_hidden(os.path.join(self.metadata_base, self.METADATA_DIR_NAME))
+        core.util.make_hidden(os.path.join(self.metadata_base, self.metadata_dir_name))
 
     @property
     def db_folder(self):
@@ -25,5 +31,5 @@ class MetadataPath():
 
     @property
     def db_folder_as_list(self):
-        return [self.metadata_base, self.METADATA_DIR_NAME, self.DB_DIR_NAME]
+        return [self.metadata_base, self.metadata_dir_name, self.DB_DIR_NAME]
 
