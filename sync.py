8eb0f7259e90e20f1a3d4f16e1bd3349d8fc3ba4
James Abel <james.c.abel@intel.com>
2014-07-16 20:43:49 -0700

    more clean up of command line argument parsing

--- sync.py
+++ sync.py
@@ -9,13 +9,11 @@ import core.metadatapath
 import core.larg
 
 if __name__ == "__main__":
-    larg_parser = core.larg.LatusArg("syncs a folder to cloud storage")
+    larg_parser = core.larg.LatusArg("syncs a folder to cloud storage", False)
     larg_parser.parser.add_argument('-l', '--local', metavar='path', required=True, help="local folder")
     larg_parser.parser.add_argument('-i', '--id', metavar='path', required=True, help="identifier for this 'node'")
     args = larg_parser.parse()
 
-    if args.verbose:
-        core.logger.set_log_level(logging.INFO)
     sync = core.sync.Sync(args.metadata, args.local, args.id)
     sync.scan()
     sync.sync()
