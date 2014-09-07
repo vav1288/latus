f9d8aa8771d9e835677c80d87cb235b8664b1b36
James Abel <j@abel.co>
2014-09-07 11:09:14 -0700

    correspond to new larg code

--- sync.py
+++ sync.py
@@ -3,7 +3,7 @@ import core.sync
 import core.larg
 
 if __name__ == "__main__":
-    larg_parser = core.larg.LatusArg("syncs a folder to cloud storage", False)
+    larg_parser = core.larg.LatusArg("securely syncs a folder to cloud storage")
     larg_parser.parser.add_argument('-l', '--local', metavar='path', required=True, help="latus folder")
     larg_parser.parser.add_argument('-c', '--cloud', metavar='path', required=True, help="cloud folder")
     args = larg_parser.parse()
