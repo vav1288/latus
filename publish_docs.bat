6c6608016fa39ff3344c924218295f7cda7b3be2
James Abel <james.c.abel@intel.com>
2014-12-08 20:39:52 -0800

    updated docs

--- publish_docs.bat
+++ publish_docs.bat
@@ -2,4 +2,5 @@ REM this seems like a kludge but it is simple
 REM all files are kept in latus and only use latusdoc to publish to the web
 copy /Y doc\*.html ..\latusdoc
 copy /Y doc\*.png ..\latusdoc
+copy /Y doc\images\*.* ..\latusdoc\images\*.*
 echo "now it is up to you do the repo commit and push on the latusdoc repo"
