3d3fb9947064b443e3be8b7faae84398286f7be0
James Abel <james.c.abel@intel.com>
2014-12-28 00:38:13 -0800

    got rid of extra 'main' window - just use system tray icon added about dialog

--- latus/gui.py
+++ latus/gui.py
@@ -12,24 +12,25 @@ import latus.const
 
 class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
 
-    def __init__(self, icon, parent=None):
+    def __init__(self, app, latus_appdata_folder, parent=None):
+        self.app = app
+        icon_path = os.path.abspath(os.path.join('icons', 'active.ico'))
+        icon = QtGui.QIcon(icon_path)
         super(SystemTrayIcon, self).__init__(icon, parent)
+        self.latus_appdata_folder = latus_appdata_folder
+
         menu = QtWidgets.QMenu(parent)
+        about_action = menu.addAction("About")
+        about_action.triggered.connect(self.about)
         exit_action = menu.addAction("Exit")
-        exit_action.triggered.connect(parent.close)
+        exit_action.triggered.connect(self.exit)
         self.setContextMenu(menu)
 
+        self.start_latus()
 
-class MainWindow(QtWidgets.QWidget):
-    def __init__(self, latus_appdata_folder):
-        super(MainWindow, self).__init__()
-        icon_path = os.path.abspath(os.path.join('icons', 'active.ico'))
-        latus.logger.log.info('icon_path : %s' % icon_path)
-        self.tray_icon = SystemTrayIcon(QtGui.QIcon(icon_path), self)
-        self.tray_icon.show()
-
-        config = latus.config.Config(latus.util.get_latus_appdata_folder())
-        latus.logger.log.info("latus_app_data: %s" % latus_appdata_folder)
+    def start_latus(self):
+        config = latus.config.Config(self.latus_appdata_folder)
+        latus.logger.log.info("latus_app_data: %s" % self.latus_appdata_folder)
 
         latus_folder = config.latus_folder_get()
         cloud_root = config.cloud_root_get()
@@ -39,14 +40,23 @@ class MainWindow(QtWidgets.QWidget):
         self.sync = latus.sync.Sync(config.crypto_get(), latus_folder, cloud_root, config.verbose_get())
         self.sync.start()
 
-    def close(self):
-        self.tray_icon.hide()
+    def show(self):
+        QtWidgets.QSystemTrayIcon.show(self)
+
+    def about(self):
+        qw = QtWidgets.QMessageBox()
+        QtWidgets.QMessageBox.about(qw, 'latus', 'www.lat.us')
+
+    def exit(self):
+        self.hide()
         self.sync.request_exit()
-        super(MainWindow, self).close()
+        QtWidgets.QApplication.exit()
 
 
 def main(latus_appdata_folder):
     latus.logger.log.info('gui mode')
     app = QtWidgets.QApplication(sys.argv)
-    w = MainWindow(latus_appdata_folder)
+    app.setQuitOnLastWindowClosed(False)  # so popup dialogs don't close the system tray icon
+    system_tray = SystemTrayIcon(app, latus_appdata_folder)
+    system_tray.show()
     sys.exit(app.exec_())
