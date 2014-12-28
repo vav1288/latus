90f2d81f43b70a43008a0f2d82f5f221790e56fb
James Abel <james.c.abel@intel.com>
2014-12-27 16:59:07 -0800

    general refactoring

--- latus/crypto.py
+++ latus/crypto.py
@@ -1,15 +1,17 @@
 
 import os
-from cryptography.fernet import Fernet
+import cryptography.fernet
+
 
 def new_key():
-    return Fernet.generate_key()
+    return cryptography.fernet.Fernet.generate_key()
+
 
 class Crypto():
     def __init__(self, key, verbose = False):
         self.__key = key
         self.__verbose = verbose
-        self.__fernet = Fernet(self.__key)
+        self.__fernet = cryptography.fernet.Fernet(self.__key)
 
     def compress(self, cwd, in_path, out_path):
         with open(os.path.join(cwd, in_path), 'rb') as in_file:
@@ -18,10 +20,17 @@ class Crypto():
                 out_file.write(token)
 
     def expand(self, cwd, in_path, out_path):
+        success = False
         with open(os.path.join(cwd, in_path), 'rb') as in_file:
-            b = self.__fernet.decrypt(in_file.read())
-            with open(out_path, 'wb') as out_file:
-                out_file.write(b)
+            try:
+                b = self.__fernet.decrypt(in_file.read())
+            except cryptography.fernet.InvalidToken:
+                b = None
+            if b:
+                with open(out_path, 'wb') as out_file:
+                    out_file.write(b)
+                    success = True
+            return success
 
 
 
