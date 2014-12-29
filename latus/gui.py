6ab5d699c31c7c64fff55490364d28c27a8038b5
James Abel <james.c.abel@intel.com>
2014-12-28 20:16:51 -0800

    add folder and crypto key edit lines

--- latus/gui.py
+++ latus/gui.py
@@ -9,6 +9,82 @@ import latus.config
 import latus.util
 import latus.const
 
+LINE_BOX_PADDING = 50
+
+class Folder():
+    def __init__(self, name, path, file_dialog_method, font_metrics):
+        self.name = name
+        self.label = QtWidgets.QLabel(self.name + " Folder:")
+        self.line = QtWidgets.QLineEdit(path)
+        self.line.setMinimumWidth(font_metrics.width(path) + LINE_BOX_PADDING)  # actual width plus padding
+        self.edit_button = QtWidgets.QDialogButtonBox()
+        self.edit_button.addButton('Edit ...', QtWidgets.QDialogButtonBox.AcceptRole)
+        self.edit_button.accepted.connect(file_dialog_method)
+
+    def layout(self, grid, column):
+        grid.addWidget(self.label, column, 0)
+        grid.addWidget(self.line, column, 1)
+        grid.addWidget(self.edit_button, column, 2)
+
+    def get(self):
+        return self.line.text()
+
+
+class CryptoKey():
+    def __init__(self, key, font_metrics):
+        self.label = QtWidgets.QLabel("Key:")
+        self.line = QtWidgets.QLineEdit(key)
+        self.line.setMinimumWidth(font_metrics.width(key) + LINE_BOX_PADDING)  # actual width plus padding
+
+    def layout(self, grid, column):
+        grid.addWidget(self.label, column, 0)
+        grid.addWidget(self.line, column, 1)
+
+    def get(self):
+        return self.line.text()
+
+
+class OptionsDialog(QtWidgets.QDialog):
+    def __init__(self, latus_appdata_folder):
+        super(OptionsDialog, self).__init__()
+
+        self.config = latus.config.Config(latus_appdata_folder)
+        self.latus_folder = Folder('Latus', self.config.latus_folder_get(), self.new_folder, self.fontMetrics())
+        self.cloud_folder = Folder('Cloud', self.config.cloud_root_get(), self.new_folder, self.fontMetrics())
+
+        self.key = CryptoKey(self.config.crypto_get_string(), self.fontMetrics())
+
+        ok_buttonBox = QtWidgets.QDialogButtonBox(QtWidgets.QDialogButtonBox.Ok)
+        ok_buttonBox.accepted.connect(self.ok)
+        cancel_buttonBox = QtWidgets.QDialogButtonBox(QtWidgets.QDialogButtonBox.Cancel)
+        cancel_buttonBox.rejected.connect(self.cancel)
+
+        #self.latus_folder.clicked.connect(self.set_latus_folder)
+        #self.cloud_root_folder.clicked.connect(self.set_cloud_root_folder)
+
+        grid_layout = QtWidgets.QGridLayout()
+        self.latus_folder.layout(grid_layout, 0)
+        self.cloud_folder.layout(grid_layout, 1)
+        self.key.layout(grid_layout, 2)
+        grid_layout.addWidget(ok_buttonBox, 3, 0)
+        grid_layout.addWidget(cancel_buttonBox, 3, 1, alignment=QtCore.Qt.AlignLeft)  # kind of cheating on the layout
+        grid_layout.setColumnStretch(1, 1)  # path column
+        self.setLayout(grid_layout)
+
+        self.setWindowTitle("Options")
+
+    def ok(self):
+        self.config.latus_folder_set(self.latus_folder.get())
+        self.config.cloud_root_set(self.cloud_folder.get())
+        self.config.crypto_set_string(self.key.get())
+        self.close()
+
+    def cancel(self):
+        self.close()
+
+    def new_folder(self):
+        f = QtWidgets.QFileDialog.getExistingDirectory()
+        return f
 
 class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
 
@@ -20,6 +96,8 @@ class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
         self.latus_appdata_folder = latus_appdata_folder
 
         menu = QtWidgets.QMenu(parent)
+        about_action = menu.addAction("Options")
+        about_action.triggered.connect(self.options)
         about_action = menu.addAction("About")
         about_action.triggered.connect(self.about)
         exit_action = menu.addAction("Exit")
@@ -44,8 +122,11 @@ class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
         QtWidgets.QSystemTrayIcon.show(self)
 
     def about(self):
-        qw = QtWidgets.QMessageBox()
-        QtWidgets.QMessageBox.about(qw, 'latus', 'www.lat.us')
+        QtWidgets.QMessageBox.about(QtWidgets.QMessageBox(), 'latus', 'www.lat.us')
+
+    def options(self):
+        options_dialog = OptionsDialog(self.latus_appdata_folder)
+        options_dialog.exec_()
 
     def exit(self):
         self.hide()
