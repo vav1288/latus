2effe87b52e725d27037377e34398140275648d1
James Abel <james.c.abel@intel.com>
2015-01-14 19:29:33 -0800

    use watchdog package (instead of our own code)
    Former-commit-id: 82c92c340b9e2e6d11088a3e6a1ff2dd315bf2a5

--- latus/sync.py
+++ latus/sync.py
@@ -1,19 +1,23 @@
 
 import os
 import glob
-import pprint
+import shutil
+
+import watchdog.observers
+import watchdog.events
+import send2trash
+
 import latus.logger
 import latus.util
 import latus.const
 import latus.walker
 import latus.hash
 import latus.crypto
-import latus.filewatcher
 import latus.fsdb
 import latus.miv
 
 
-class Sync():
+class Sync(watchdog.events.FileSystemEventHandler):
     """
     Determines what needs to be done to sync local to cloud.
     """
@@ -43,27 +47,32 @@ class Sync():
         latus.util.make_dirs(self.cloud_miv_folder)
 
     def start(self):
-        self.sync()
-        self.latus_file_watcher = latus.filewatcher.FileWatcher(self.latus_folder, self.sync)
-        self.latus_file_watcher.start()
-        self.cloud_file_watcher = latus.filewatcher.FileWatcher(self.cloud_folder, self.sync)
-        self.cloud_file_watcher.start()
+        self.sync_local()
+        self.sync_other()
+
+        self.latus_observer = watchdog.observers.Observer()
+        self.latus_observer.schedule(self, self.latus_folder, recursive=True)
+        self.latus_observer.start()
+        self.cloud_observer = watchdog.observers.Observer()
+        self.cloud_observer.schedule(self, self.cloud_folder, recursive=True)
+        self.cloud_observer.start()
 
     def request_exit(self):
         latus.logger.log.info('request_exit begin')
-        self.latus_file_watcher.request_exit()
-        self.cloud_file_watcher.request_exit()
-        self.latus_file_watcher.join(60)
-        self.cloud_file_watcher.join(60)
+
+        self.latus_observer.stop()
+        self.cloud_observer.stop()
+        self.latus_observer.join()
+        self.cloud_observer.join()
+
         latus.logger.log.info('request_exit end')
 
-    # todo: break this up into 2 routines - local changes and cloud changes
-    def sync(self):
-        """
-        Sync new or updated files (both local and cloud).
-        """
+    def dispatch(self, event):
+        self.sync_local()
+        self.sync_other()
 
-        latus.logger.log.info('scanning : %s' % self.latus_folder)
+    def sync_local(self):
+        latus.logger.log.info('%s : scanning local %s' % (self.node_id, self.latus_folder))
 
         crypto = latus.crypto.Crypto(self.crypto_key, self.verbose)
 
@@ -71,33 +80,41 @@ class Sync():
         local_walker = latus.walker.Walker(self.latus_folder)
         for partial_path in local_walker:
             # this is where we use the local _file_ name to create the cloud _folder_ where the fernet and metadata reside
-            full_path = local_walker.full_path(partial_path)
-            hash, _ = latus.hash.calc_sha512(full_path)
+            local_full_path = local_walker.full_path(partial_path)
+            hash, _ = latus.hash.calc_sha512(local_full_path)
             if hash:
                 # todo: encrypt the hash?
                 cloud_fernet_file = os.path.join(self.cloud_cache_folder, hash + self.fernet_extension)
                 fs_db = latus.fsdb.FileSystemDB(self.cloud_fs_db_folder, self.node_id, True)
                 most_recent_hash = fs_db.get_most_recent_hash(partial_path)
                 if hash != most_recent_hash:
-                    mtime = os.path.getmtime(full_path)
-                    size = os.path.getsize(full_path)
+                    mtime = os.path.getmtime(local_full_path)
+                    size = os.path.getsize(local_full_path)
+                    latus.logger.log.info('%s : updated %s' % (self.node_id, local_full_path))
                     fs_db.update(latus.miv.next_miv(self.cloud_miv_folder), partial_path, size, hash, mtime)
                 fs_db.close()
                 if not os.path.exists(cloud_fernet_file):
-                    latus.logger.log.info('writing %s (%s)' % (partial_path, cloud_fernet_file))
+                    latus.logger.log.info('%s : writing %s (%s)' % (self.node_id, partial_path, cloud_fernet_file))
                     crypto.compress(self.latus_folder, partial_path, os.path.abspath(cloud_fernet_file))
             else:
-                latus.logger.log.warn('no hash for %s' % full_path)
+                latus.logger.log.warn('no hash for %s' % local_full_path)
 
         # deleted local files
         fs_db = latus.fsdb.FileSystemDB(self.cloud_fs_db_folder, self.node_id, True)
         for partial_path in fs_db.get_paths():
             db_info = fs_db.get_file_info(partial_path)
             cloud_most_recent_hash = db_info[-1]['hash']
-            if not cloud_most_recent_hash and os.path.exists(os.path.join(self.latus_folder, partial_path)):
+            latus_path = os.path.join(self.latus_folder, partial_path)
+            if cloud_most_recent_hash and not os.path.exists(latus_path):
+                latus.logger.log.info('%s : found as deleted %s' % (self.node_id, latus_path))
                 # mark in DB as deleted
                 fs_db.update(latus.miv.next_miv(self.cloud_miv_folder), partial_path, None, None, None)
         fs_db.close()
+
+    def sync_other(self):
+        latus.logger.log.info('%s : scanning other nodes' % self.node_id)
+
+        crypto = latus.crypto.Crypto(self.crypto_key, self.verbose)
 
         # new or updated cloud files
         this_fs_db = latus.fsdb.FileSystemDB(self.cloud_fs_db_folder, self.node_id)
@@ -105,7 +122,7 @@ class Sync():
             file_name = os.path.basename(db_file)
             db_node_id = file_name.split('.')[0]
             if db_node_id != self.node_id:
-                latus.logger.log.info('db_node_id %s' % db_node_id)
+                latus.logger.log.info('%s : checking node %s' % (self.node_id, db_node_id))
                 other_fs_db = latus.fsdb.FileSystemDB(self.cloud_fs_db_folder, db_node_id)
                 for partial_path in other_fs_db.get_paths():
                     local_path = os.path.join(self.latus_folder, partial_path)
@@ -119,7 +136,7 @@ class Sync():
                     if db_info:
                         most_recent = db_info[-1]
 
-                    if most_recent and most_recent['size'] > 0:
+                    if most_recent and most_recent['size'] and most_recent['size'] > 0:
                         local_sequence = None
                         local_file_info = this_fs_db.get_file_info(partial_path)
                         if local_file_info:
@@ -129,5 +146,13 @@ class Sync():
                                 (local_hash != most_recent['hash'] and most_recent['seq'] > local_sequence):
                             cloud_fernet_file = os.path.abspath(os.path.join(self.cloud_cache_folder, most_recent['hash'] + self.fernet_extension))
                             file_abs_path = os.path.abspath(os.path.join(self.latus_folder, partial_path))
+                            latus.logger.log.info('%s : %s has changed %s , so propagating to %s' % (self.node_id, db_node_id, partial_path, file_abs_path))
                             expand_ok = crypto.expand(cloud_fernet_file, file_abs_path)
-
+                    if most_recent and most_recent['hash'] is None:
+                        latus.logger.log.info('%s : %s has deleted %s , so deleting %s' % (self.node_id, db_node_id, partial_path, local_path))
+                        try:
+                            if os.path.exists(local_path):
+                                send2trash.send2trash(local_path)
+                        except OSError:
+                            # fallback
+                            latus.logger.log.warn('%s : send2trash failed on %s' % (self.node_id, local_path))
