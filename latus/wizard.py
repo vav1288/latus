3728f0c04332e6d91a2da9e3850de9cedbc049e3
James Abel <j@abel.co>
2015-02-15 23:48:28 -0800

    basics of wizard gui
    Former-commit-id: 15c643bbe2bebed91aac53b77eb15316dca98585

--- latus/wizard.py
+++ latus/wizard.py
@@ -1,6 +1,8 @@
 
 import os
 import win32api
+import threading
+import time
 
 import latus.util
 import latus.const
@@ -8,30 +10,33 @@ import latus.const
 # setup wizard
 
 
-class FolderWizard():
+class FolderWizard(threading.Thread):
+
     def __init__(self):
+        super().__init__()
         self.potential_cloud_folders = []
-        self.selected_cloud_folder = None
         self.selected_latus_folder = None
+        self.found_alert = self.default_found_alert
+        self.progress = self.default_progress
+        self.request_exit_flag = False
+
+    def default_found_alert(self, _):
+        print('default_found_alert')
+
+    def default_progress(self, _):
+        print('default_progress')
+
+    def set_found_alert(self, method):
+        self.found_alert = method
+
+    def set_progress(self, method):
+        self.progress = method
 
     def get_potential_cloud_folders(self):
         return self.potential_cloud_folders
 
-    def set_cloud_folder(self, folder):
-        self.selected_cloud_folder = folder
-
-    def set_latus_folder(self, folder):
-        self.selected_latus_folder = folder
-
-    def get_cloud_folder(self):
-        return self.selected_cloud_folder
-
-    def get_latus_folder(self):
-        return self.selected_latus_folder
-
-    # child classes can override this to get progress
-    def progress(self, path):
-        print(path)
+    def request_exit(self):
+        self.request_exit_flag = True
 
     def try_dropbox_folder(self, candidate_path):
         # dropbox appears to use this layout ...
@@ -39,41 +44,51 @@ class FolderWizard():
         if os.path.exists(candidate) and os.path.exists(os.path.join(candidate, '.dropbox')):
             if candidate not in self.potential_cloud_folders:
                 self.potential_cloud_folders.append(candidate)
+                if self.found_alert:
+                    self.found_alert(self.potential_cloud_folders)
 
-    def find_cloud_folders(self, exhaustive=False):
+    def run(self):
         self.potential_cloud_folders = []
         home_folder = os.path.expanduser('~')
         # first, try the normal location
         self.try_dropbox_folder(home_folder)
-        if exhaustive or len(self.potential_cloud_folders) == 0:
-            # Search more possible locations (will take longer).
-            roots = []
-            if latus.util.is_windows():
-                drives = win32api.GetLogicalDriveStrings().split('\0')[:-1]
-                for drive in drives:
-                    root = os.path.join(drive, os.sep)
-                    if os.path.exists(os.path.join(root, 'Windows')):
-                        # only look in the user area for the drive that has Windows, Program Files, etc.
-                        roots.append(home_folder)
-                    else:
-                        roots.append(root)
-            elif latus.util.is_linux():
-                pass  # todo: get possible location roots for Linux
-            for root in roots:
-                self.try_dropbox_folder(root)
-                self.progress(root)
-                for path, dirs, _ in os.walk(root):
-                    for d in dirs:
-                        candidate = os.path.join(path, d)
-                        self.try_dropbox_folder(candidate)
-                        self.progress(candidate)
-        return self.potential_cloud_folders
+
+        # Search more possible locations (will take longer).
+        roots = []
+        if latus.util.is_windows():
+            drives = win32api.GetLogicalDriveStrings().split('\0')[:-1]
+            for drive in drives:
+                root = os.path.join(drive, os.sep)
+                if os.path.exists(os.path.join(root, 'Windows')):
+                    # only look in the user area for the drive that has Windows, Program Files, etc.
+                    roots.append(home_folder)
+                else:
+                    roots.append(root)
+        elif latus.util.is_linux():
+            pass  # todo: get possible location roots for Linux
+        for root in roots:
+            self.try_dropbox_folder(root)
+            for path, dirs, _ in os.walk(root):
+                for d in dirs:
+                    if self.request_exit_flag:
+                        return
+                    candidate = os.path.join(path, d)
+                    self.try_dropbox_folder(candidate)
+                    self.progress(candidate)
+        self.progress(None)
 
 
 def latus_folder_from_cloud_folder(path):
     return os.path.join(os.path.dirname(path), latus.const.NAME)
 
 if __name__ == "__main__":
+    def my_alert(folders):
+        print(folders)
+
     w = FolderWizard()
-    print(w.find_cloud_folders())
-    print(w.find_cloud_folders(True))
+    w.set_found_alert(my_alert)
+    w.start()
+    while w.is_alive():
+        time.sleep(0.1)
+    print('Done!')
+    print(w.get_potential_cloud_folders())
--- latus/wizardgui.py
+++ latus/wizardgui.py
@@ -1,34 +1,45 @@
 
+import time
+
 from PyQt5 import QtGui, QtWidgets, QtCore
 
 import latus.wizard
 import latus.config
 
+CLOUD_FOLDER_STRING = 'cloud_folder'
+LATUS_FOLDER_STRING = 'latus_folder'
 
 class GUIWizard(QtWidgets.QWizard):
 
     def __init__(self):
-        super(GUIWizard, self).__init__()
-        app_folder_wizard = latus.wizard.FolderWizard()
-        self.cloud_folder_list = QtWidgets.QListWidget()
+        super().__init__()
+
+        self.folder_wizard = latus.wizard.FolderWizard()
 
         self.addPage(IntroPage())
-        self.addPage(CloudFolderPage(app_folder_wizard, self.cloud_folder_list))
-        self.addPage(LatusFolderPage(app_folder_wizard, self.cloud_folder_list))
+        self.addPage(CloudFolderPage(self.folder_wizard))
+        self.addPage(LatusFolderPage())
         self.addPage(ConclusionPage())
-        self.setWindowTitle("")
+        self.setWindowTitle("Latus Setup")
         self.show()
 
     def accept(self):
-        print(self.cloud_folder_list.currentItem().text())
-        print(self.field('latus_folder'))
+        cloud_folder = self.field(CLOUD_FOLDER_STRING)
+        if cloud_folder:
+            print(CLOUD_FOLDER_STRING, cloud_folder)
+        latus_folder = self.field(LATUS_FOLDER_STRING)
+        if latus_folder:
+            print(LATUS_FOLDER_STRING, latus_folder)
         super(GUIWizard, self).accept()
 
+    def done(self, result):
+        self.folder_wizard.request_exit()
+        super(GUIWizard, self).done(result)
 
 class IntroPage(QtWidgets.QWizardPage):
 
     def __init__(self):
-        super(IntroPage, self).__init__()
+        super().__init__()
         self.setTitle("Latus Setup Wizard")
 
         label = QtWidgets.QLabel("This will guide you through the Latus setup process.")
@@ -41,57 +52,88 @@ class IntroPage(QtWidgets.QWizardPage):
 
 class CloudFolderPage(QtWidgets.QWizardPage):
 
-    def __init__(self, folder_wizard, cloud_folder_list):
-        super(CloudFolderPage, self).__init__()
+    def __init__(self, folder_wizard):
+        super().__init__()
+        self.cloud_folder_list = QtWidgets.QListWidget()
         self.folder_wizard = folder_wizard
-        self.cloud_folder_list = cloud_folder_list
         self.cloud_folders = []
+        self.prior_time = 0
+
+        self.folder_wizard.set_found_alert(self.wizard_alert)
+        self.folder_wizard.set_progress(self.progress)
+        self.folder_wizard.start()
 
         self.setTitle("Cloud storage folder")
-        self.setSubTitle("Please select the cloud storage folder on your computer (for example: Dropbox, Microsoft's "
-                         "OneDrive, Google Drive, etc.):")
+        self.setSubTitle("We will now attempt to automatically find your cloud storage folder.  This is the folder you "
+                         "use for Dropbox, Microsoft's OneDrive, Google Drive, etc.\n\nYou don't have to wait for the "
+                         "search to complete.  Once the cloud storage folder is displayed you can select it and go to "
+                         "the next page.  Note that it may take a while to search your entire computer.")
 
         self.cloud_folder_list.show()
 
+        self.progressbar = QtWidgets.QLineEdit()
+        self.progressbar.setReadOnly(True)
+        self.progressbar.setText('...')
+
         layout = QtWidgets.QGridLayout()
-        layout.addWidget(self.cloud_folder_list, 0, 1)
+        layout.addWidget(self.progressbar, 0, 0)
+        layout.addWidget(self.cloud_folder_list, 1, 0)
         self.setLayout(layout)
 
-    def initializePage(self):
+        # use this for the value that's been selected
+        self.cloud_folder_line = QtWidgets.QLineEdit()
+        self.registerField(CLOUD_FOLDER_STRING, self.cloud_folder_line)
+
+    def progress(self, d):
+        t = time.time()
+        if d:
+            if t - self.prior_time > 0.2:
+                self.progressbar.setText('Searching: ' + d)
+                self.progressbar.setCursorPosition(0)
+                self.prior_time = t
+        else:
+            self.progressbar.setText('Search complete.')
+
+    def wizard_alert(self, folders):
         item_count = 0
-        for folder in self.folder_wizard.find_cloud_folders():
-            item = QtWidgets.QListWidgetItem()
-            item.setText(folder)
-            self.cloud_folder_list.insertItem(item_count, item)
+        for folder in folders:
+            item_string = self.cloud_folder_list.item(item_count)
+            if not item_string:
+                self.cloud_folder_list.insertItem(item_count, folder)
             item_count += 1
+
+    def validatePage(self):
+        if self.cloud_folder_list.currentItem():
+            self.cloud_folder_line.setText(self.cloud_folder_list.currentItem().text())
+        return super().validatePage()
 
 
 class LatusFolderPage(QtWidgets.QWizardPage):
 
-    def __init__(self, folder_wizard, cloud_folder_list):
-        super(LatusFolderPage, self).__init__()
-        self.folder_wizard = folder_wizard
-        self.cloud_folder_list = cloud_folder_list
+    def __init__(self):
+        super().__init__()
+        self.latus_folder_box = QtWidgets.QLineEdit()
+        self.latus_folder_box.setReadOnly(True)
 
         self.setTitle("Latus folder")
         self.setSubTitle("This is the Latus folder on your computer:")
 
-        self.latus_folder_box = QtWidgets.QLineEdit()
-        self.latus_folder_box.setReadOnly(True)
         self.latus_folder_box.show()
 
         layout = QtWidgets.QGridLayout()
         layout.addWidget(self.latus_folder_box, 0, 1)
         self.setLayout(layout)
 
+        self.registerField(LATUS_FOLDER_STRING, self.latus_folder_box)
+
     def initializePage(self):
-        latus_folder = latus.wizard.latus_folder_from_cloud_folder(self.cloud_folder_list.currentItem().text())
+        latus_folder = latus.wizard.latus_folder_from_cloud_folder(self.field('cloud_folder'))
         self.latus_folder_box.setText(latus_folder)
-        self.registerField('latus_folder', self.latus_folder_box)
+
 
 class ConclusionPage(QtWidgets.QWizardPage):
     def __init__(self):
-        super(ConclusionPage, self).__init__()
+        super().__init__()
 
         self.setTitle("Complete!")
 
