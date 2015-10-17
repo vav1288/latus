57526c721320a07d2f7080dcee5f5c1f4784e343
James Abel <j@abel.co>
2015-10-17 10:51:31 -0700

    move to server based miv

--- latus/miv.py
+++ latus/miv.py
@@ -1,91 +1,38 @@
 import os
 import datetime
 
-import sqlalchemy
-import sqlalchemy.exc
-import sqlalchemy.ext.declarative
+#import sqlalchemy
+#import sqlalchemy.exc
+#import sqlalchemy.ext.declarative
+
+import requests
+import time
+import logging
 
 import latus.logger
-import latus.const
-import latus.util
+#import latus.const
+#import latus.util
 
 """
     Monotonically Increasing Value (miv)
 """
 
 
-Base = sqlalchemy.ext.declarative.declarative_base()
+def get_miv():
+    # todo: allow us to use mono.lat.us .  Will have to deal with name servers, etc.
+    server = 'http://monotonic-latus.rhcloud.com/'
+    r = requests.get(server)
+    if r.status_code == 200:
+        miv = float(r.text.split()[0])
+        latus.logger.log.info('miv from %s : %s' % (server, miv))
+    else:
+        # the monotonic value from the server is based on time(), so if we can use the server use our local time
+        latus.logger.log.warn('miv: could not get time from server %s - using local time()' % server)
+        miv = time.time()
+    return miv
 
-
-class MonotonicallyIncreasingValue():
-    def __init__(self, folder):
-        self.database_file_name = 'miv' + latus.const.DB_EXTENSION
-        sqlite_file_path = os.path.join(folder, self.database_file_name)
-        self.db_engine = sqlalchemy.create_engine('sqlite:///' + os.path.abspath(sqlite_file_path))
-        sa_metadata = sqlalchemy.MetaData()
-        self.conn = self.db_engine.connect()
-        self.miv_table = sqlalchemy.Table('miv', sa_metadata,
-                                          sqlalchemy.Column('value', sqlalchemy.Integer, primary_key=True),
-                                          sqlalchemy.Column('timestamp', sqlalchemy.DateTime),
-                                          )
-        sa_metadata.create_all(self.db_engine)
-        if self.get() is None:
-            # initialize
-            latus.logger.log.info('init miv')
-            command = self.miv_table.insert().values(value=0, timestamp=datetime.datetime.utcnow())
-            self.conn.execute(command)
-
-    def next(self):
-        command = self.miv_table.update().values(value=self.miv_table.c.value + 1, timestamp=datetime.datetime.utcnow())
-        increment_complete = False
-        retry_count = 3
-        while not increment_complete and retry_count > 0:
-            try:
-                self.conn.execute(command)
-                increment_complete = True
-            except sqlalchemy.exc.IntegrityError:
-                latus.logger.log.warn('retrying miv next (retry_count %s)' % retry_count)
-            retry_count -= 1
-        miv_value = self.get()
-        latus.logger.log.info('miv : %s' % miv_value)
-        return miv_value
-
-    def get(self):
-        miv_value = None
-        result = self.conn.execute(self.miv_table.select())
-        if result:
-            row = result.fetchone()
-            if row:
-                miv_value = row[0]
-        return miv_value
-
-    def close(self):
-        self.conn.close()
-
-
-def next_miv(folder):
-    """
-    Return next monotonically increasing value.  Returns None if it could not be determined.
-    :param folder: folder where the sqlite db file resides
-    :return: a monotonically increasing integer (or None on error)
-    """
-    value = None
-    time_out_counter = 0
-    while value is None and time_out_counter < 10:
-        time_out_counter += 1
-        # In case the DB is locked, wait and retry (in a loop).
-        # If this fails it seems to take a few seconds to do so, so each iteration is a few seconds.
-        try:
-            miv = MonotonicallyIncreasingValue(folder)
-            value = miv.next()
-            miv.close()
-        except sqlalchemy.exc.OperationalError:
-            latus.logger.log.info('db retry %s' % time_out_counter)
-            latus.util.wait_random_avg_1_sec()
-    if value is None:
-        latus.logger.log.error('monotonically increasing value could not be determined')
-    return value
 
 if __name__ == '__main__':
     latus.logger.init('temp')
-    print(next_miv('temp'))
+    latus.logger.set_console_log_level(logging.INFO)
+    print(get_miv())
