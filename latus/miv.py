4eecd15465c930844feef56c2a3bb2f6f46cd686
James Abel <j@abel.co>
2015-10-18 10:47:24 -0700

    added fault tolerance

--- latus/miv.py
+++ latus/miv.py
@@ -1,17 +1,10 @@
-import os
-import datetime
-
-#import sqlalchemy
-#import sqlalchemy.exc
-#import sqlalchemy.ext.declarative
 
 import requests
 import time
 import logging
 
 import latus.logger
-#import latus.const
-#import latus.util
+
 
 """
     Monotonically Increasing Value (miv)
@@ -19,16 +12,28 @@ import latus.logger
 
 
 def get_miv():
-    # todo: allow us to use mono.lat.us .  Will have to deal with name servers, etc.
-    server = 'http://monotonic-latus.rhcloud.com/'
-    r = requests.get(server)
-    if r.status_code == 200:
-        miv = float(r.text.split()[0])
-        latus.logger.log.info('miv from %s : %s' % (server, miv))
-    else:
-        # the monotonic value from the server is based on time(), so if we can use the server use our local time
-        latus.logger.log.warn('miv: could not get time from server %s - using local time()' % server)
-        miv = time.time()
+    server = 'http://mono.lat.us'
+    miv = None
+    tries = 0
+    while miv is None and tries < 3:
+        r = requests.get(server)
+        if r.status_code == 200:
+            # do a 'try' in case we get some strange text back
+            try:
+                miv = float(r.text)
+            except ValueError:
+                miv = None
+            if miv:
+                latus.logger.log.info('miv from %s : %s' % (server, str(miv)))
+                return miv
+            else:
+                latus.logger.log.warn('try %d : unexpected text from %s : %s' % (tries, server, r.text))
+        tries += 1
+
+    # the monotonic value from the server is based on time(), so if we can't use the server then use our local time
+    miv = time.time()
+    latus.logger.log.warn('could not get miv from %s - using local time() : %s' % (server, str(miv)))
+
     return miv
 
 
