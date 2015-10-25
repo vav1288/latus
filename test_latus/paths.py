c79ea6247b756ba3455153ca3124dbe1be41220f
James Abel <j@abel.co>
2015-10-24 20:58:30 -0700

    USB or c: drive

--- test_latus/paths.py
+++ test_latus/paths.py
@@ -1,9 +1,15 @@
 
 import os
 
+
 def get_data_root():
+
     # todo: make this some sort of configuration
-    return os.path.abspath(os.path.join('j:', os.sep, 'James', 'Projects', 'latus', 'test_latus', 'data'))
+    #projects_root = os.path.join('j:', os.sep, 'James', 'Projects', )
+    projects_root = os.path.join(os.sep, 'Users', 'james', 'Documents', 'james', 'projects')
+
+    return os.path.abspath(os.path.join(projects_root, 'latus', 'test_latus', 'data'))
+
 
 def root_test_gui_wizard():
     return os.path.join('test_latus', 'test_gui_wizard')
