534d870c1c1b9bc57b463de3d9aa2cd22583b3b1
James Abel <j@abel.co>
2015-10-24 21:00:06 -0700

    add pex

--- make_venv.bat
+++ make_venv.bat
@@ -9,10 +9,14 @@ pip.exe install pathlib
 pip.exe install watchdog
 pip.exe install rsa
 pip.exe install pytest
-pip.exe install PySide
 pip.exe install dirsync
 pip.exe install python-dateutil
 pip.exe install requests
+pip.exe install pex
 popd
 REM packages not in pypi (can not be installed with pip):
 venv\Scripts\easy_install.exe third_party_installers\pywin32-219.win-amd64-py3.4.exe
+REM
+REM PySide not installing this way so use PySide-1.2.2.win-amd64-py3.4.exe
+REM pip.exe install PySide
+third_party_installers\PySide-1.2.2.win-amd64-py3.4.exe
