deb7a1fd836d8281f58a1b55cbfc937832909dbb
James Abel <j@abel.co>
2015-11-04 14:36:31 -0800

    add PyAutoGUI

--- make_venv.bat
+++ make_venv.bat
@@ -13,10 +13,11 @@ pip.exe install dirsync
 pip.exe install python-dateutil
 pip.exe install requests
 pip.exe install pex
+pip.exe install PyAutoGUI
 popd
 REM packages not in pypi (can not be installed with pip):
 venv\Scripts\easy_install.exe third_party_installers\pywin32-219.win-amd64-py3.4.exe
 REM
 REM PySide not installing this way so use PySide-1.2.2.win-amd64-py3.4.exe
 REM pip.exe install PySide
-third_party_installers\PySide-1.2.2.win-amd64-py3.4.exe
+venv\Scripts\easy_install.exe third_party_installers\PySide-1.2.2-py3.4-win-amd64.egg
