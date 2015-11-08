2133b74e6359e43a4cae7ae2e0e4c050a8d94dc4
James Abel <j@abel.co>
2015-11-08 10:51:28 -0800

    use activate

--- make_venv.bat
+++ make_venv.bat
@@ -1,6 +1,5 @@
 c:\Python34\python c:\Python34\Tools\Scripts\pyvenv.py --clear venv
-pushd .
-cd venv\Scripts
+call venv\Scripts\activate.bat
 pip.exe install cryptography
 pip.exe install sqlalchemy
 pip.exe install send2trash
@@ -12,12 +11,10 @@ pip.exe install pytest
 pip.exe install dirsync
 pip.exe install python-dateutil
 pip.exe install requests
-pip.exe install pex
 pip.exe install PyAutoGUI
-popd
 REM packages not in pypi (can not be installed with pip):
-venv\Scripts\easy_install.exe third_party_installers\pywin32-219.win-amd64-py3.4.exe
+easy_install.exe third_party_installers\pywin32-219.win-amd64-py3.4.exe
 REM
 REM PySide not installing this way so use PySide-1.2.2.win-amd64-py3.4.exe
 REM pip.exe install PySide
-venv\Scripts\easy_install.exe third_party_installers\PySide-1.2.2-py3.4-win-amd64.egg
+easy_install.exe third_party_installers\PySide-1.2.2-py3.4-win-amd64.egg
