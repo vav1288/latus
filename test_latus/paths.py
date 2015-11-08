9da184b8add0c94befe53d38ef4667e9f08feae8
James Abel <j@abel.co>
2015-11-08 10:52:08 -0800

    make data root relative

--- test_latus/paths.py
+++ test_latus/paths.py
@@ -3,12 +3,7 @@ import os
 
 
 def get_data_root():
-
-    # todo: make this some sort of configuration
-    #projects_root = os.path.join('j:', os.sep, 'James', 'Projects', )
-    projects_root = os.path.join(os.sep, 'Users', 'james', 'Documents', 'james', 'projects')
-
-    return os.path.abspath(os.path.join(projects_root, 'latus', 'test_latus', 'data'))
+    return os.path.abspath(os.path.join('test_latus', 'data'))
 
 
 def root_test_gui_wizard():
