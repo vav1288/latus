4f1c1ccbb831a201e27b02c09ca3e96fb4462b08
james abel <j@abel.co>
2016-05-29 21:50:42 -0700

    add appdirs

--- make_venv.bat
+++ make_venv.bat
@@ -10,6 +10,7 @@ pip install requests
 pip install python-dateutil
 pip install pytest
 pip install dirsync
+pip install appdirs
 REM packages that can not be installed with pip:
 easy_install.exe third_party_installers\pywin32-220.win-amd64-py3.4.exe
 pip.exe install third_party_installers\PySide-1.2.2-cp34-none-win_amd64.whl
