dfaf24cff0df6fdf20585e15b39d6449ea7de369
james abel <j@abel.co>
2016-09-10 18:39:49 -0700

    fix key file UI

--- latus/key_management.py
+++ latus/key_management.py
@@ -8,20 +8,26 @@ import platform
 import latus.const
 import latus.logger
 
-from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import QFileDialog, QApplication
 
 
 def write_latus_key_gui(key):
-    key_file_directory = QFileDialog.getExistingDirectory(None, 'Save Latus key file:')
-    key_file_path = os.path.join(key_file_directory, latus.const.LATUS_KEY_FILE_NAME)
-    if key_file_path and len(key_file_path) > 0:
-        write_latus_key_to_file(key, key_file_path)
+    dialog_return = QFileDialog.getSaveFileName(caption='Save Latus key file:',
+                                                directory=os.path.join(os.getcwd(), getpass.getuser() + latus.const.LATUS_KEY_FILE_EXT),
+                                                filter=latus.const.LATUS_KEY_FILE_EXT)
+    if dialog_return and len(dialog_return) > 0:
+        key_file_path = dialog_return[0]
+        if key_file_path and len(key_file_path) > 0:
+            write_latus_key_to_file(key, key_file_path)
+            return key_file_path
+    latus.logger.log.warn('key_file_path not provided by dialog box')
+    return None
 
 
 def read_latus_key_gui():
     key = None
     key_file_name = QFileDialog.getOpenFileName(None, 'Latus key file:',
-                                                          filter=latus.const.LATUS_KEY_FILE_NAME)
+                                                filter=latus.const.LATUS_KEY_FILE_EXT)
     if key_file_name and len(key_file_name[0]) > 0:
         key_record = read_latus_key_from_file(key_file_name[0])
         if key_record:
@@ -44,3 +50,8 @@ def read_latus_key_from_file(latus_key_file_path):
         key = json.load(f)
     return key
 
+if __name__ == '__main__':
+    latus.logger.init('temp')
+    print(latus.logger.get_log_folder())
+    app = QApplication([''])
+    write_latus_key_gui(b'dummy_key')
