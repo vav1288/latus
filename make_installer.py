8500852fd545ab8bd00b04f894795f32400e0564
james abel <j@abel.co>
2016-12-07 19:29:34 -0800

    remove unnecessary param

--- make_installer.py
+++ make_installer.py
@@ -4,11 +4,11 @@ import osnap.installer
 import latus
 
 
-def make_installer(verbose):
+def make_installer():
     osnap.installer.make_installer(latus.__python_version__, latus.__application_name__, latus.__author__,
                                    'access all your files across all your computers - secure, free and open source',
                                    'www.lat.us')
 
 
 if __name__ == '__main__':
-    make_installer(True)
+    make_installer()
