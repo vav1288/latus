dbc25a2261d70edd21774aae58b98a273b15c841
james abel <j@abel.co>
2017-02-18 16:01:12 -0800

    use new way of creating venv

--- make_venv.sh
+++ make_venv.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-/usr/local/bin/pyvenv --clear venv
+python3 -m venv --clear venv
 venv/bin/pip install -U pip
 venv/bin/pip install -U setuptools
 venv/bin/pip install -r requirements.txt
