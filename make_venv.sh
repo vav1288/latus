eaa63373286f0970f16bd40eb3c2573be1a173cb
james abel <j@abel.co>
2017-02-20 11:57:10 -0800

    add pyautogui

--- make_venv.sh
+++ make_venv.sh
@@ -3,3 +3,11 @@ python3 -m venv --clear venv
 venv/bin/pip install -U pip
 venv/bin/pip install -U setuptools
 venv/bin/pip install -r requirements.txt
+#
+# testing tools - these can be ignored when creating the production release - i.e. the frozen executables
+venv/bin/pip install -U coverage
+# pyautogui will not install properly from requirements.txt
+venv/bin/pip install -U pyobjc-core
+venv/bin/pip install -U pyobjc
+venv/bin/pip install -U pyautogui
+#
