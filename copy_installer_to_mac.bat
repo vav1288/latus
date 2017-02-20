bc65f851883701e11205c06d3de33d6b913f4282
james abel <j@abel.co>
2017-02-20 12:11:17 -0800

    initial check-in

---
+++ copy_installer_to_mac.bat
@@ -0,0 +1,6 @@
+REM
+REM workaround that copies the installer over the Mac side of this computer
+REM so it can be uploaded to AWS since the Windows side does not have
+REM AWS credentials installed
+REM
+copy installers\latus_installer.exe y:\projects\latus\build
