28759645d7dbc2cf4ca1c75a3ac6285f94fe483c
james abel <j@abel.co>
2017-02-20 18:47:37 -0800

    New style of venv creation

--- make_venv.bat
+++ make_venv.bat
@@ -1,4 +1,4 @@
-\Users\james\AppData\Local\Programs\Python\Python35\python.exe \Users\james\AppData\Local\Programs\Python\Python35\Tools\scripts\pyvenv.py --clear venv
+\Users\james\AppData\Local\Programs\Python\Python36\python.exe -m venv --clear venv
 venv\Scripts\pip3 install -U pip
 venv\Scripts\pip3 install -U setuptools
 venv\Scripts\pip3 install -r requirements.txt
