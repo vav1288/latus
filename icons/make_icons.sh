df89a790e07f9e5ae3e465a5f67a0d8cb2781d8d
james abel <j@abel.co>
2017-03-10 22:44:26 -0800

    complete mac icons

---
+++ icons/make_icons.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# https://developer.apple.com/library/content/documentation/GraphicsAnimation/Conceptual/HighResolutionOSX/Optimizing/Optimizing.html
+# icon_1022_1016.png is the original full resolution source
+mkdir icon.iconset
+sips -z 16 16     icon_1022_1016.png --out icon.iconset/icon_16x16.png
+sips -z 32 32     icon_1022_1016.png --out icon.iconset/icon_16x16@2x.png
+sips -z 32 32     icon_1022_1016.png --out icon.iconset/icon_32x32.png
+sips -z 64 64     icon_1022_1016.png --out icon.iconset/icon_32x32@2x.png
+sips -z 128 128   icon_1022_1016.png --out icon.iconset/icon_128x128.png
+sips -z 256 256   icon_1022_1016.png --out icon.iconset/icon_128x128@2x.png
+sips -z 256 256   icon_1022_1016.png --out icon.iconset/icon_256x256.png
+sips -z 512 512   icon_1022_1016.png --out icon.iconset/icon_256x256@2x.png
+sips -z 512 512   icon_1022_1016.png --out icon.iconset/icon_512x512.png
+sips -z 1024 1024 icon_1022_1016.png --out icon.iconset/icon_512x512@2x.png
+cp icon_1022_1016.png icon.png
+iconutil -c icns icon.iconset
+../venv/bin/pyrcc5 -o ../latus/icons.py -no-compress icon.qrc
+rm -R icon.iconset
