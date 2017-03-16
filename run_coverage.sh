a46a50e85dee6eb4101e837eb80124c691cbf8c9
james abel <j@abel.co>
2017-03-16 16:35:17 -0700

    explain how to use this coverage technique

--- run_coverage.sh
+++ run_coverage.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #
+# this uses the pytest coverage files (run pytest first)
 mkdir temp/coverage
 venv/bin/coverage report > temp/coverage/report.txt
 venv/bin/coverage annotate -d temp/coverage
