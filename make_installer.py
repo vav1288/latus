8030fa6097f3c735ee920c996e01141eb99e7149
james abel <j@abel.co>
2017-03-16 16:34:46 -0700

    add build timestamp

--- make_installer.py
+++ make_installer.py
@@ -1,10 +1,18 @@
 
+import os
+import datetime
+
 import osnap.installer
 
 import latus
 
 
 def make_installer():
+    with open(os.path.join(latus.__application_name__, 'build.py'), 'w') as f:
+        f.write('# programmatically generated - do not edit\n')
+        f.write('\n')
+        f.write('BUILD_TIMESTAMP = "%s"\n' % datetime.datetime.utcnow())
+
     osnap.installer.make_installer(latus.__python_version__, latus.__application_name__, latus.__version__,
                                    latus.__author__,
                                    'access all your files across all your computers - secure, free and open source',
