bd60e8b5d0dcaf3d5b2fe3046d1c265387ddc815
james abel <j@abel.co>
2017-08-16 00:44:03 -0700

    refactor aws

--- test_latus/conftest.py
+++ test_latus/conftest.py
@@ -3,7 +3,8 @@ import pytest
 
 import test_latus
 import test_latus.tstutil
-import latus.aws
+
+import latus.aws.aws_access
 import latus.aws.util.init_aws_db
 import latus.logger
 
@@ -15,10 +16,8 @@ def session_setup():
 
 @pytest.fixture(scope="module")
 def module_setup():
+    latus.logger.init('temp')
     cc = test_latus.tstutil.get_cloud_config()
     if cc['aws_local']:
-        latus.aws.local_testing()
-        print('*** dropall ***', latus.aws.dynamo_db_endpoint_url)
-        if latus.logger.log:
-            latus.logger.log.warn('*** dropall ***')
-        latus.aws.util.init_aws_db.aws_drop_all(latus.aws.dynamo_db_endpoint_url)
+        latus.aws.util.init_aws_db.aws_drop_all()
+        latus.aws.aws_access.init(True)
